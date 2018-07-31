41d0034a0f7e1816f42926d2b2cc23758b61fc6a
Rama Pradana <ramapradana67@hotmail.com>
2018-07-31 12:49:58 +0800

    add twitter and facebook feed

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -37,6 +37,15 @@ ITCC 2018
 @endsection
 
 @section('intro')
+<script id="twitter-wjs" type="text/javascript" async defer src="//platform.twitter.com/widgets.js"></script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v3.1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <!--==========================
     Hero Section
 ============================-->
@@ -814,11 +823,12 @@ ITCC 2018
 
 	        <div class="col-sm-12 col-md-4 twitter">
 	            <h3><b>Twitter</b> Feed</h3>
-	            <a class="twitter-timeline" data-width="360" data-height="500" data-theme="light" data-link-color="#021b79" href="https://twitter.com/ITCCUdayana" style="color: #0575e6;">Tweets by ITCCUdayana</a>
+	            <a class="twitter-timeline" data-width="360" data-height="500" data-theme="light" data-link-color="#021b79" href="https://twitter.com/itcc_udayana" style="color: #0575e6;">Tweets by ITCCUdayana</a>
 	        </div>
 
           	<div class="col-sm-12 col-md-4 facebook">
             	<h3><b>Facebook</b> Feed</h3>
+
             	<div class="fb-page" data-href="https://www.facebook.com/ITCC.Udayana/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ITCC.Udayana/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ITCC.Udayana/" style="color: #0575e6;">ITCC Udayana</a></blockquote></div>
                  
           	</div>
